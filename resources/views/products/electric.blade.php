47403506d284c17efe044a5c65d6ea0013a04d3e
Jesse Fender <jf0772256@otc.edu>
2017-05-01 10:04:40 -0500

    updated all product files tith user route

--- resources/views/products/electric.blade.php
+++ resources/views/products/electric.blade.php
@@ -81,7 +81,7 @@
                 </div>
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/user') }}">Home</a>
                     @else
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
