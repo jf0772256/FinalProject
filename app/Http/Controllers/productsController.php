7d1b679b8f22ba52a3fb572e5645eb2c1dafa2f7
Tyler Poag <tymo.mr@gmail.com>
2017-05-01 09:48:56 -0500

    Added old controller for products
    Products controller reupped

---
+++ app/Http/Controllers/productsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class productsController extends Controller
+{
+
+  public function cars()
+  {
+        return view('products/cars');
+  }
+
+  public function trucks()
+    {
+          return view('products/trucks');
+    }
+    public function suv()
+      {
+            return view('products/suv');
+      }
+      public function compacts()
+        {
+              return view('products/compacts');
+        }
+        public function electric()
+          {
+                return view('products/electric');
+          }
+}
