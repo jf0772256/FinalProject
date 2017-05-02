f103e8d281f8c9ca0fcc67b70987efcec43c5104
Jesse Fender <jf0772256@otc.edu>
2017-05-01 20:55:47 -0500

    #1 Resolved :)

--- resources/views/partial/nav.blade.php
+++ resources/views/partial/nav.blade.php
@@ -1,13 +1,15 @@
 @if (Route::has('login'))
-<div class="top-left links">
+<div class="top-left links nav-link-padding">
   <a href="/">Home</a>
     <a href="/cars">Cars</a>
     <a href="/trucks">Trucks</a>
     <a href="/suv">SUV</a>
     <a href="/compacts">Compacts</a>
     <a href="/electric">Electric/Hybrid</a>
-    @include('partial.theme')
 </div>
+
+    @include('partial.theme')
+
     <div class="top-right links">
         @if (Auth::check())
             <a href="{{ url('/user') }}">Home</a>
