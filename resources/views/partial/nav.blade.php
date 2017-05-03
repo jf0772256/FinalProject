8d6359b70efd7cedb3c3108fc3294f1b765a830e
Tyler Poag <tymo.mr@gmail.com>
2017-05-03 10:05:42 -0500

    changed dark theme/ altered nav structure
    *** Nav structure is broken ***

--- resources/views/partial/nav.blade.php
+++ resources/views/partial/nav.blade.php
@@ -1,30 +1,71 @@
-<div class="top-left links nav-link-padding">
-    <a href="{{url('/')}}">Home</a>
-    <a href="/cars">Cars</a>
-    <a href="/trucks">Trucks</a>
-    <a href="/suv">SUV</a>
-    <a href="/compacts">Compacts</a>
-    <a href="/electric">Electric/Hybrid</a>
-</div>
+
+<nav class="navbar navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="nav navbar-right">
+        @include('partial.theme')
+        <div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav ">
+              <li>  <a href="{{url('/')}}">Home</a> </li>
+              <li>  <a href="/cars">Cars</a> </li>
+              <li>  <a href="/trucks">Trucks</a> </li>
+              <li>  <a href="/suv">SUV</a> </li>
+              <li>  <a href="/compacts">Compacts</a> </li>
+              <li>  <a href="/electric">Electric/Hybrid</a> </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+  </div>
+</nav>
 
 @if (Route::has('login'))
-    {{-- <div class="top-left links nav-link-padding">
-        <a href="/">Home</a>
-        <a href="/cars">Cars</a>
-        <a href="/trucks">Trucks</a>
-        <a href="/suv">SUV</a>
-        <a href="/compacts">Compacts</a>
-        <a href="/electric">Electric/Hybrid</a>
-    </div> --}}
+    {{-- <nav class="navbar navbar-fixed-top">
+      <div class="container-fluid">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                  </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav> --}}
 
-    @include('partial.theme')
 
-    <div class="top-right links">
+    <nav class="navbar navbar-fixed-top">
+
+      <div class="container-fluid">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                  <li>  <a href="{{url('/')}}">Home</a> </li>
+                  <li>  <a href="/cars">Cars</a> </li>
+                  <li>  <a href="/trucks">Trucks</a> </li>
+                  <li>  <a href="/suv">SUV</a> </li>
+                  <li>  <a href="/compacts">Compacts</a> </li>
+                  <li>  <a href="/electric">Electric/Hybrid</a> </li>
+
+            <!-- /.navbar-collapse -->
+            <!-- Collect the nav links, forms, and other content for toggling -->
+
         @if (Auth::check())
-            <a href="{{ url('/user') }}">Home</a>
+            <li ><a href="{{ url('/user') }}">Home</a> </li>
         @else
-            <a href="{{ url('/login') }}">Login</a>
-            <a href="{{ url('/register') }}">Register</a>
+            <li><a href="{{ url('/login') }}">Login</a> </li>
+            <li> <a href="{{ url('/register') }}">Register</a> </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
         @endif
-    </div>
-@endif
+  </div>
+</nav>
+      @endif
