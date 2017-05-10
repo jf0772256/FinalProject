52e95f228fbe168406e07d33592e54164429bbf8
Tyler Poag <tymo.mr@gmail.com>
2017-05-10 08:30:24 -0500

    Updated the nav
    and updated the composer files

--- resources/views/partial/nav.blade.php
+++ resources/views/partial/nav.blade.php
@@ -1,27 +1,4 @@
 
-<nav class="navbar navbar-fixed-top">
-  <div class="container-fluid">
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="nav navbar-right">
-        @include('partial.theme')
-        <div>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav ">
-              <li>  <a href="{{url('/')}}">Home</a> </li>
-              <li>  <a href="/cars">Cars</a> </li>
-              <li>  <a href="/trucks">Trucks</a> </li>
-              <li>  <a href="/suv">SUV</a> </li>
-              <li>  <a href="/compacts">Compacts</a> </li>
-              <li>  <a href="/electric">Electric/Hybrid</a> </li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
-  </div>
-</nav>
 
 @if (Route::has('login'))
     {{-- <nav class="navbar navbar-fixed-top">
