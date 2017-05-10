351e2d08fa4e97f0e17f2f6cb5b758facdbb2043
Jesse Fender <jf0772256@otc.edu>
2017-05-10 09:52:25 -0500

    Starting the code to generate a selct element on the page

--- app/categories.php
+++ app/categories.php
@@ -11,6 +11,19 @@ class Categories extends Model
     protected function getCategories(){
       $cat_vals=[];
       $cat_vals = DB::table('categories')->pluck('categoryType');
-      var_dump($cat_vals);
+      return $this;
+    }
+
+    protected function buildSelector($data){
+      //returns the internals of the selector
+      return $this;
+    }
+
+    protected function createSelector($selectorName){
+        return $this;
+    }
+
+    protected function closeSelector(){
+      return $this;
     }
 }
