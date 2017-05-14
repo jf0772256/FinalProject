0d88f3d6abcaddf1f5622d9f1761d6d3f6bf5580
Jesse Fender <jf0772256@otc.edu>
2017-05-14 14:19:24 -0500

    more to add

--- app/categories.php
+++ app/categories.php
@@ -7,23 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categories extends Model
 {
-    //
-    protected function getCategories(){
-      $cat_vals=[];
-      $cat_vals = DB::table('categories')->pluck('categoryType');
-      return $this;
-    }
+    //we need to get all the categories from the database and display them in a drop box for the admin to select from
+    // to do this we wil use non chainable linking as this was causing compatibility issues.
 
-    protected function buildSelector($data){
-      //returns the internals of the selector
-      return $this;
-    }
-
-    protected function createSelector($selectorName){
-        return $this;
-    }
-
-    protected function closeSelector(){
-      return $this;
+    public static function makeSelectorCat($sName){
+      //the function that should make and then return the selector drop box.
+      $iCnt = 0;
+      $data = DB::table('categories')->select('categoryType')->get();
+      echo "<div class='form-group'><select name='$sName'>";
+      echo "<option value='-1'>Select A Category</option>";
+      foreach ($data as $value) {
+        $item = $value->categoryType;
+        echo "<option value='$iCnt'>$item</option>";
+        $iCnt++;
+      }
+      echo "</select></div>";
     }
 }
